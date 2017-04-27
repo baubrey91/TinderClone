9088e51e92b1017a5904db5d23ec3606f84e5ad1
Brandon Aubrey <brandonaubrey@brandons-mbp-2>
2017-04-26 19:48:28 -0700

    7:48

--- TinderClone/CardsViewController.swift
+++ TinderClone/CardsViewController.swift
@@ -10,16 +10,40 @@ import UIKit
 
 class CardsViewController: UIViewController {
 
+    var center: CGPoint?
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func onPanGesture(_ panGestureRecognizer: UIPanGestureRecognizer) {
+        
+        center = imageView.center
+        
+        let translation = panGestureRecognizer.translation(in: view)
+        let velocity = panGestureRecognizer.velocity(in: view)
+        
+        if panGestureRecognizer.state == .began {
+            
+        }
+        
+        if panGestureRecognizer.state == .changed {
+            imageView.center = CGPoint(x: imageView.center.x + translation.x, y: center!.y)
 
+        }
+        
+        if panGestureRecognizer.state == .ended {
 
+            imageView.center = center!
+        }
+    }
 }
 
